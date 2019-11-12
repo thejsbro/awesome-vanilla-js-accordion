bb0a0733b1adbf0e7729cdfe58fd0defc510da36
thejsbro <thejsbro@gmail.com>
2019-11-12 17:57:56 +0300

    Updates

--- src/scripts/const.js
+++
@@ -1,7 +0,0 @@
-export const accordionClasses = {
-    CONTAINER: 'accordion',
-    TITLE: 'accordion-title',
-    CONTENT: 'accordion-content',
-    ACTIVE: 'is-active',
-    LOADING: 'loading'
-};
